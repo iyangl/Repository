12e6e9578913c859e28c067a011db6ccc28de49c
leolee-798 <ly798vv@163.com>
2017-07-10 09:51:54 +0800

    新增

---
+++ dailyZhiHuDemo/app/src/main/java/com/ly/example/myapplication2/widgets/behavior/DetailImageBehavior.java
@@ -0,0 +1,13 @@
+package com.ly.example.myapplication2.widgets.behavior;
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.util.AttributeSet;
+
+public class DetailImageBehavior extends AppBarLayout.ScrollingViewBehavior {
+
+    public DetailImageBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
--- utils/README.md
+++ utils/README.md
@@ -6,3 +6,10 @@
 ### HtmlUtil.java：  
 	根据url动态生成加载css、js标签  
 
+### Divider.java
+	一个封装的ItemDecoration，包括显示纯色divider、显示图片divider、divider的上下左右的间距、宽高设置
+	来源：[开源实验室](https://blog.kymjs.com/code/2016/07/10/01/ "开源实验室")
+
+### RecyclerItemClickListener.java
+	一个 RecyclerView 使用的 OnItemClickListener
+	来源：[开源实验室](https://blog.kymjs.com/code/2016/07/10/01/ "开源实验室")
